2d92d55547f6db9bb9cc99b613c199cd9980b816
Yash290402 <yash56774@gmail.com>
2024-03-19 19:27:43 +0530

    starting backend with appwrite

---
+++ 12MegaBlog/src/conf/conf.js
@@ -0,0 +1,11 @@
+const conf = {
+
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatebaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwritecoolectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwritebucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+    
+}
+
+export default conf
