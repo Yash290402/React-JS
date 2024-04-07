ab7252aafb7f24268b9aeb5271aba1358587c2db
Yash290402 <yash56774@gmail.com>
2024-04-07 14:39:51 +0530

    until authentication layout

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -3,11 +3,15 @@ import Footer from "./footer/Footer"
 import Container from "./container/Container"
 import Logo from "./Logo"
 import Logout from "./header/Logout"
+import Button from './Button'
+import Input from './Input'
 
 export{
     Header,
     Footer,
     Container,
     Logo,
-    Logout
+    Logout,
+    Button,
+    Input
 }
